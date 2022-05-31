92a14a12d5dea5c147993a381d98bb2a9c0ceef5
Mohammad Nabi Noorzai <mnoorzai21@gmail.com>
2022-05-30 17:00:35 -0700

    Initial set up and created navigation.js a bootstrap navigation bar

--- src/App.js
+++ src/App.js
@@ -1,5 +1,14 @@
+import React from "react";
+import Navigation from "./components/Navigation";
+
 function App() {
-  return <h1>Portfolio</h1>;
+  return (
+    <div>
+      <Navigation />
+      <h1>Portfolio</h1>
+      <button className="btn-primary"> Click Me </button>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,7 @@
+import React from "react";
+import { Navbar, Nav, Container } from "react-bootstrap";
+import "./Navigation.css";
+
+export default function Navigation(props) {
+  return <div>My header goes here!</div>;
+}
