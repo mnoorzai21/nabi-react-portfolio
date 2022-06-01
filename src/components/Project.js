d90d3fcbfd53b0d67bfeecbc0938063de377a058
Mohammad Nabi Noorzai <mnoorzai21@gmail.com>
2022-06-01 09:01:27 -0700

    Update PortfolioContainer and Project

--- src/components/Project.js
+++ src/components/Project.js
@@ -1,21 +1,17 @@
-import React from "react";
 import "../styles/Project.css";
 
-export default function Project(id, image, alt, title, details, demo, repo) {
+function Project(title, image, imageSrc, details, deployedLink, repo) {
   return (
     <div className="container-fluid d-flex flex-row flex-wrap justify-content-center">
       return (
-      <div
-        key={id}
-        className="card bg-dark text-white custom-card text-center m-3"
-      >
-        <img src={image} className="card-img custom-img" alt={alt} />
+      <div className="card bg-dark text-white custom-card text-center m-3">
+        <img src={image} className="card-img custom-img" alt={imageSrc} />
         <div className="card-img-overlay p-4 custom-card-info">
           <h5 className="card-title">{title}</h5>
           <p className="card-text">{details}</p>
           <a
             className="btn btn-success btn-floating m-1"
-            href={demo}
+            href={deployedLink}
             role="button"
           >
             Demo
@@ -33,3 +29,4 @@ export default function Project(id, image, alt, title, details, demo, repo) {
     </div>
   );
 }
+export default Project;
--- src/components/pages/PortfolioContainer.js
+++ src/components/pages/PortfolioContainer.js
@@ -1,3 +1,23 @@
 import Project from "../Project";
 
-const myPorjects = {};
+const myPorjects = {
+  GitFit: {
+    title: "Git-Fit",
+    details: "Fitness app, that generates a workout with exercises/set/reps",
+    deployedLink: "https://peaceful-dusk-53929.herokuapp.com/",
+    github: "https://github.com/ShawnArgent/Git-Fit",
+  },
+  Golfapedia: {
+    title: "Golfapedia",
+    details: "Golfing app, that finds nearby golf courses",
+    deployedLink: "https://nirav-v.github.io/golfapedia/",
+    github: "https://github.com/nirav-v/golfapedia",
+  },
+  passwordGenerator: {
+    title: "password-generator",
+    details: "Passwork generator app, generates a random password",
+    deployedLink: "https://mnoorzai21.github.io/password-generator-mnn/",
+    github: "https://github.com/mnoorzai21/password-generator-mnn",
+  },
+};
+export default myPorjects;
