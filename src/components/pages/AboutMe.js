7d4d77c7e8bb1db63df94dc8275a010ac17a49ba
Nabi Noorzai <mnoorzai21@gmail.com>
2024-09-11 20:08:52 -0700

    Update about me

--- src/components/pages/AboutMe.js
+++ src/components/pages/AboutMe.js
@@ -16,16 +16,42 @@ export default function AboutMe() {
         </div>
       </div>
       <p className="load about-me p-4">
-        I am a Computer Science student at San Diego State University, equipped
-        with an Associate's degree in computer science from San Diego Mesa
-        College and a certificate in web development from the University of
-        California, San Diego. Known for my exceptional problem-solving skills,
-        quick learning abilities, and a strong aptitude for working in
-        high-pressure situations, I am driven by a deep passion to bring
-        real-world projects to life from their conceptual stages. With a
-        commitment to continuous growth and a relentless pursuit of excellence,
-        I am dedicated to making a meaningful impact in the field of computer
-        science.
+        <br>
+          As a dedicated Computer Science student at San Diego State University,
+          graduating in Spring 2025, I bring a wealth of knowledge and practical
+          experience to the field. My educational background includes an
+          Associate's degree in Computer Science from San Diego Mesa College and
+          a certificate in Web Development from the University of California,
+          San Diego.
+        </br>
+        <br>
+          I have developed a strong foundation across various areas of computer
+          science, including software developing, software engineering, machine
+          learning, artificial intelligence, computer security, and data
+          science. My expertise extends to applying advanced algorithms and data
+          structures to solve complex challenges.
+        </br>
+        <br>
+          Known for my exceptional problem-solving skills and quick learning
+          abilities, I thrive in high-pressure environments and am adept at
+          swiftly mastering new concepts and technologies. My commitment to
+          excellence is matched by my work ethic; I approach every project with
+          dedication, ensuring thorough and high-quality outcomes.
+        </br>
+        <br>
+          I excel in collaborative settings, valuing teamwork and the collective
+          input of diverse perspectives to drive innovative solutions. My
+          ability to work effectively within teams and engage in constructive
+          collaboration enhances my projects and fosters a productive working
+          environment.
+        </br>
+        <br>
+          Driven by a passion for transforming conceptual ideas into impactful
+          real-world applications, I am committed to continuous growth and
+          professional development. I am eager to contribute my skills and
+          enthusiasm to advance the field of computer science and make a
+          significant impact through my work.
+        </br>
       </p>
     </div>
   );
