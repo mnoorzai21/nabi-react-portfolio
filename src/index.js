e340f8e4145055288eb5f4a44b0f65df31180fb4
Nabi Noorzai <mnoorzai21@gmail.com>
2024-09-12 00:08:49 -0700

    fix buggs

--- src/index.js
+++ src/index.js
@@ -1,8 +1,15 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
+import ReactDOM from "react-dom";
 import App from "./App";
 import "bootstrap/dist/css/bootstrap.css";
 import "./index.css";
+import reportWebVitals from "./reportWebVitals";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<App />);
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById("root")
+);
+
+reportWebVitals();
