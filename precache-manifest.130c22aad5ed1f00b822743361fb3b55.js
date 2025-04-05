cd67b8c40bb6b5428800f14d6f358170a198c52e
Mohammad Nabi Noorzai <mnoorzai21@gmail.com>
2025-04-05 00:24:29 -0700

    Updates

---
+++ precache-manifest.130c22aad5ed1f00b822743361fb3b55.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c7d46be4f3c3cff4b3f5db6ca46f611a",
+    "url": "/nabi-react-portfolio/index.html"
+  },
+  {
+    "revision": "db7e53d4d1ccadb569ef",
+    "url": "/nabi-react-portfolio/static/css/2.04b36542.chunk.css"
+  },
+  {
+    "revision": "70fd96a7642c92f46e0d",
+    "url": "/nabi-react-portfolio/static/css/main.1d5c0598.chunk.css"
+  },
+  {
+    "revision": "db7e53d4d1ccadb569ef",
+    "url": "/nabi-react-portfolio/static/js/2.b06da317.chunk.js"
+  },
+  {
+    "revision": "6cb2abd69bcf5c1bc8ea13bc6a30ce0e",
+    "url": "/nabi-react-portfolio/static/js/2.b06da317.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "70fd96a7642c92f46e0d",
+    "url": "/nabi-react-portfolio/static/js/main.df09df78.chunk.js"
+  },
+  {
+    "revision": "be16317e4a3c3a76ed78",
+    "url": "/nabi-react-portfolio/static/js/runtime-main.b695beca.js"
+  },
+  {
+    "revision": "b9503e1c2337861935112b1b8cbc032e",
+    "url": "/nabi-react-portfolio/static/media/Nabis_Resume.b9503e1c.pdf"
+  },
+  {
+    "revision": "89205dce870212bad5fe377cffaab489",
+    "url": "/nabi-react-portfolio/static/media/background.89205dce.png"
+  },
+  {
+    "revision": "a31b88b3a3bba4100e2c6747787479cd",
+    "url": "/nabi-react-portfolio/static/media/bookSearch-screenshot.a31b88b3.png"
+  },
+  {
+    "revision": "7a3600e6fb02ef7a448c2ecb5e39fd4d",
+    "url": "/nabi-react-portfolio/static/media/monitorList-screenshot.7a3600e6.png"
+  },
+  {
+    "revision": "ac2a0fe4d95625733c8ff056f24b25b8",
+    "url": "/nabi-react-portfolio/static/media/myphoto.ac2a0fe4.jpg"
+  },
+  {
+    "revision": "53341d709d39499508da6818730a3476",
+    "url": "/nabi-react-portfolio/static/media/password-generator.53341d70.png"
+  },
+  {
+    "revision": "573476273b34a93e5666855703626020",
+    "url": "/nabi-react-portfolio/static/media/project-1.57347627.png"
+  },
+  {
+    "revision": "58e5f6ea489562904d51d5081ee2cd92",
+    "url": "/nabi-react-portfolio/static/media/project-2.58e5f6ea.jpeg"
+  },
+  {
+    "revision": "3bc8df0b9bb33bde960bc4feaef9a5e6",
+    "url": "/nabi-react-portfolio/static/media/weather-dashboard.3bc8df0b.png"
+  }
+]);
