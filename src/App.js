32976ca41278f27f6c810c275a0fa892e24e1736
Mohammad Nabi Noorzai <mnoorzai21@gmail.com>
2025-04-05 23:20:02 -0700

    Updated my css

--- src/App.js
+++ src/App.js
@@ -1,11 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./components/Home";
+import AboutMe from "./components/pages/AboutMe";
+import Portfolio from "./components/pages/Portfolio";
+import Contact from "./components/pages/Contact";
+import Resume from "./components/pages/Resume";
+import Navigation from "./components/Navigation";
 
 function App() {
+  const [currentPage, setCurrentPage] = useState("Home");
+
+  const handlePageChange = (page) => setCurrentPage(page);
+
   return (
-    <main>
-      <Home />
-    </main>
+    <Router>
+      <Navigation
+        currentPage={currentPage}
+        handlePageChange={handlePageChange}
+      />
+
+      <main>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/aboutme" element={<AboutMe />} />
+          <Route path="/projects" element={<Portfolio />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/resume" element={<Resume />} />
+        </Routes>
+      </main>
+    </Router>
   );
 }
 
