1d9e6a175a4c3e33848924cf97539c490e882207
Mohammad Nabi Noorzai <mnoorzai21@gmail.com>
2025-04-05 23:49:28 -0700

    Updated my css

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -54,7 +54,7 @@ export default function Contact() {
 
   return (
     <section className="row text-light -align-center col-9 justify-content-left">
-      <div className="col-12 col-md-10 col-lg-12">
+      <div className="col-12 col-md-6 col-lg-6">
         <form className="p-4 rounded" onSubmit={handleFormSubmit}>
           <h2>Contact</h2>
 
