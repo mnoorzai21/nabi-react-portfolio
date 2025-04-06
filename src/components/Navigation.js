0c7e155f5f6eebd5914a4d83e887b63ad6bbed30
Mohammad Nabi Noorzai <mnoorzai21@gmail.com>
2025-04-05 23:22:21 -0700

    Updated my css

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -16,16 +16,15 @@ export default function Navigation(props) {
       collapseOnSelect
       expand="sm"
       variant="dark"
-      className="bg-transparent py-3">
+      className="bg-transparent py-3"
+    >
       <Container className="d-flex justify-content-between align-items-center">
         <Navbar.Brand as={Link} to="/" className="text-light fs-4 fw-bold">
           Mohammad Nabi Noorzai
         </Navbar.Brand>
 
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-        <Navbar.Collapse
-          id="responsive-navbar-nav"
-          className="justify-content-end">
+        <Navbar.Collapse id="responsive-navbar-nav" className="justify-content-end">
           <Nav>
             <Link to="/aboutme" className={navLinkClass("/aboutme")}>
               About Me
