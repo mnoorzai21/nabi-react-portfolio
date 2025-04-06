cfa9d07ac365dd9ccea5a805f4b5ccd299d2efd0
Mohammad Nabi Noorzai <mnoorzai21@gmail.com>
2025-04-05 20:57:33 -0700

    Updated my css

--- src/App.js
+++ src/App.js
@@ -3,9 +3,9 @@ import Home from "./components/Home";
 
 function App() {
   return (
-    <div>
+    <main>
       <Home />
-    </div>
+    </main>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -4,5 +4,10 @@ import App from "./App";
 import "bootstrap/dist/css/bootstrap.css";
 import "./index.css";
 
+// React 18 root rendering with StrictMode enabled for development
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<App />);
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
