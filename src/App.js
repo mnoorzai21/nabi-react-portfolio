282a7ef03e68440009e3a950d10c667279e127d2
Mohammad Nabi Noorzai <mnoorzai21@gmail.com>
2025-04-05 22:55:07 -0700

    Updated my css

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import AboutMe from "./components/pages/AboutMe";
 import Portfolio from "./components/pages/Portfolio";
 import Contact from "./components/pages/Contact";
